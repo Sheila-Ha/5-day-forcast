6768593085bd5fb54227e23592b850c5eec82a6c
sheilahanson <slhanson11@live.com>
2023-12-07 17:12:02 -0600

    js, added var btn search, function gitApi, fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,25 @@
 //Your API key is 8d43b91350520bddbdbe05651cf109c0
 //endpoint api.openweathermap.org for your API calls
 
+// var APIKey="8d43b91350520bddbdbe05651cf109c0";
+// var queryURL="https://home.openweathermap.org/api_keys"
 
-var APIKey="8d43b91350520bddbdbe05651cf109c0";
-var queryURL="https://home.openweathermap.org/api_keys"
+var searchBtn = document.querySelector('.search-btn');
+
+//add event listener
+searchBtn.addEventListener('click', function() {
+  featchWeatherApi();
+})
+
+function gitApi() {
+  //fetch request
+  var requestURL = 'https://home.openweathermap.org/api_keys';
+
+  fetch(requestURL)
+    .then(function (response) {
+      return response.jason();
+    })
+    .then(function (data) {
+      console.log(data)
+    })
+}
